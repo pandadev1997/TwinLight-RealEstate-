ed3a82432e7af501c1b4e82de3454d841ea8c04f
debas <debas@LAPTOP-B8G85UAF>
2023-05-31 16:43:50 +0530

    commit 2nd

--- com.real-state.twinlight/src/test/java/dataProvider_Second/Dataprovider_fetch_outsideTest.java
+++ com.real-state.twinlight/src/test/java/dataProvider_Second/Dataprovider_fetch_outsideTest.java
@@ -6,6 +6,6 @@ public class Dataprovider_fetch_outsideTest {
 	@Test(dataProviderClass = Dataprovider_outside_class.class,dataProvider = "data")
 	public void getData(String phone,int price,int rating,String feedback) {
 		System.out.println(phone+"-->"+price+"-->"+rating+" -->"+feedback);
-		
+		//we learned a new topic
 	}
 }
